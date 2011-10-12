65e780d87c4db8cdd5b5826962500f040849541c
David Schoen <dave@lyte.id.au>
2011-10-12 23:12:18 +1100

    refactoring tests to work with no external procs

--- tests/reverse.js
+++ tests/reverse.js
@@ -3,62 +3,62 @@ var conf = require('./helpers/config')
 
 module.exports = testCase({
     setUp: function (callback) {
-        callback();
-    },
-    tearDown: function (callback) {
-        // clean up
-        callback();
-    },
-    testStartDefaultServer: function (test) {
 		var util = require('util');
-		var http_proxy = require('../lib/proxy')
+		var proxy = require('../lib/proxy')
 		var fs = require('fs')
 
-		var proxy = http_proxy.createServer(
-			{
-				"listen": {
-					"hostname": "127.0.0.1",
-					"port": conf.ports.init
-				},
-				"proxy": {
-					"name": "node-proxy",
-					"origins": [
-						{
-							"host": "google.com",
-							"port": 80
-						}
-					]
-				},
-				"cache": {
-					"memory": {
-						"maxitems": 1000000,
-						"maxmem": 128,
-						// TODO: if cleanup is > 0 then it does a setInterval that is not closed by proxy.close()... this is probably a bug
-						"cleanup": 0,
-						"stats": 0
-					},
-					"disk": {
-						"path": "helpers/cache"
-					},
+		// set up basic web server
+		var http = require('http').createServer(function (req, res) {
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.end('world\n');
+		});
+
+		var proxy = proxy.createServer({
+			"name": "node-proxy",
+			"origins": [
+				{
+					"host": "127.0.0.1",
+					"port": conf.ports.webserver
+				}
+			],
+			"cache": {
+				"memory": {
+					"maxitems": 1000000,
+					"maxmem": 128,
+					// TODO: if cleanup is > 0 then it does a setInterval that is not closed by proxy.close()... this is probably a bug
+					"cleanup": 0,
 					"stats": 0
 				},
-				"accesslog": {
-					"path": "helpers/access_log"
-				}
+				"disk": {
+					"path": "helpers/cache"
+				},
+				"stats": 0
+			},
+			"accesslog": {
+				"path": "helpers/access_log"
 			}
-		);
-		
-		proxy.listen(conf.ports.init, '127.0.0.1', function() {});
+		});
 
-		this.proxy=proxy;
-		// explicitly confirm type
-		test.ok(this.proxy instanceof require('../lib/reverseProxy').Server);
+		this.http = http;
+		this.proxy = proxy;
 
-		this.proxy.close();
-
-        test.done();
+		http.listen(conf.ports.webserver, "127.0.0.1", function() {
+			proxy.listen(conf.ports.proxy, '127.0.0.1', function() {
+				callback();
+			});
+		});
+	},
+    tearDown: function (callback) {
+        // clean up
+		this.proxy.close()
+		this.http.close()
+        callback();
     },
-
+	testProxyType: function(test) {
+		test.ok(this.proxy instanceof require('../lib/reverseProxy').Server,
+			'Proxy has incorrect type');
+		test.done()
+	},
 	testHelloWorld: function(test) {
 		var http = require('http')
 		var client = http.createClient(conf.ports.proxy, '127.0.0.1');
--- tests/yum.js
+++ tests/yum.js
@@ -3,13 +3,6 @@ var conf = require('./helpers/config')
 
 module.exports = testCase({
     setUp: function (callback) {
-        callback();
-    },
-    tearDown: function (callback) {
-        // clean up
-        callback();
-    },
-    testStartServer: function (test) {
 		var util = require('util');
 		var http_proxy = require('../lib/proxy')
 		var fs = require('fs')
@@ -24,34 +17,45 @@ module.exports = testCase({
 			}
 		);
 		
-		proxy.listen(conf.ports.init, '127.0.0.1', function() {});
+		proxy.listen(conf.ports.init, '127.0.0.1', callback);
 
 		this.proxy=proxy;
+    },
+    tearDown: function (callback) {
+        // clean up
+		this.proxy.close();
+
+        callback();
+    },
+    testStartServer: function (test) {
 		// explicitly confirm type
 		test.ok(this.proxy instanceof require('../lib/yumProxy').Server);
-
-		this.proxy.close();
 
         test.done();
     },
 	testGetRPM: function(test) {
-		//var http = require('http')
-		//var client = http.createClient(conf.ports.yumproxy, '127.0.0.1');
+		// timeout test (avoid deadlocks)
+		setTimeout(function() {
+			throw("test appears to be deadlocked")
+		}, 1000);
+		var http = require('http')
+		var client = http.createClient(conf.ports.init, '127.0.0.1');
 
-		//var req = client.request('GET', '/yum/foo.rpm', {});
+		var req = client.request('GET', '/yum/foo.rpm', {});
 
-		//req.end();
-		//req.on('response', function(res) {
-		//	test.equal('200', res.statusCode);
-		//	res.on('data', function(chunk) {
-		//		if (!this.body) this.body = '';
-		//		this.body += chunk;
-		//	});
-		//	res.on('end', function() {
-		//		test.equal(this.body, 'world\n');
-		//		test.done();
-		//	});
-		//});
-		test.done()
+		req.end();
+		req.on('response', function(res) {
+			test.equal('200', res.statusCode);
+			res.on('data', function(chunk) {
+				console.log("got data")
+				if (!this.body) this.body = '';
+				this.body += chunk;
+			});
+			res.on('end', function() {
+				console.log("got end");
+				test.equal(this.body, 'world\n');
+				test.done();
+			});
+		});
 	}
 });
