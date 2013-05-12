0a7324cc6d9b06632470b1286662f20e932fb3af
David Schoen <dave@lyte.id.au>
2013-05-12 15:08:52 +1000

    get rid of wrench

--- spec/lib/cacheFile.spec.js
+++ spec/lib/cacheFile.spec.js
@@ -8,7 +8,6 @@ describe('CacheFile', function() {
 	var Fixture = require('../fixture');
 	var expect = Fixture.expect;
 	var FS = require('q-io/fs');
-	var wrench = require('wrench');
 	var cacheDir = Fixture.cacheDir;
 	var Q = require('q');
 
@@ -18,10 +17,20 @@ describe('CacheFile', function() {
 	// https://github.com/kriskowal/q-io/pull/32
 
 	beforeEach(function() {
-		wrench.rmdirSyncRecursive(cacheDir + '/', true);
-		return FS.makeTree(cacheDir)
-		.then(function() {
+		return FS.isDirectory(cacheDir)
+		.then(function(isDir) {
+			if (isDir) return FS.removeTree(cacheDir);
+		}).then(function() {
+			return FS.makeTree(cacheDir);
+		}).then(function() {
 			return FS.makeTree(cacheDir + '/data');
+		});
+	});
+
+	afterEach(function() {
+		return FS.isDirectory(cacheDir)
+		.then(function(isDir) {
+			if (isDir) return FS.removeTree(cacheDir);
 		});
 	});
 
@@ -37,21 +46,24 @@ describe('CacheFile', function() {
 			var cacheFile = new CacheFile(cacheDir, 'somefile');
 			return cacheFile.getWriter()
 			.then(function(writer) {
-				return writer.write("foo")
+				return writer.write("baz")
 				.then(function() {
 					return writer.close();
 				});
 			}).then(function() {
 				return expect(
 					FS.read(cacheDir + '/data/somefile')
-				).to.become("foo");
+				).to.become("baz");
 			});
 		});
 	});
 
 	describe("with an existing file", function() {
 		beforeEach(function() {
-			return FS.write(cacheDir + '/data/somefile', 'foo');
+			return FS.makeTree(cacheDir + '/data')
+			.then(function() {
+				return FS.write(cacheDir + '/data/somefile', 'foo');
+			});
 		});
 
 		it("knows file exists", function() {
